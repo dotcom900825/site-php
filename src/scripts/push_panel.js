5f4d29474a35791481ce137b4001b967680be4bf
qdx <qdxzzz@gmail.com>
2013-08-09 00:48:54 -0700

    make the form load dynamically with the select box using ajax, make discardChange button just refresh the page, added todos

--- src/scripts/push_panel.js
+++ src/scripts/push_panel.js
@@ -1,3 +1,4 @@
+//saving the card may not need ajax call?
 function save($par){
     console.log($par);
 //    var logoText = $("#logoText-wrapper>input").val()
@@ -9,7 +10,7 @@ function save($par){
 
     //});
 }
-
+//TODO: validate the form
 function validator(name, value){
     switch(name){
         case "logoText":
@@ -17,13 +18,13 @@ function validator(name, value){
         default:
     }
 }
-
+// TODO: need add cardId param to send to push.php
 function push(){
     console.log("push");
 
     $.ajax({
         type : 'POST',
-        url : "./../../lib/ws/push.php?test=true",
+        url : "./../../lib/ws/push.php",
         beforeSend : function() {
         },
         success : function(result) {
@@ -34,14 +35,35 @@ function push(){
 
 
 }
-
+//TODO: maybe this is not needed? or add a button to do this?
 function saveAndPush(){
     console.log("save and push");
     save();
     push();
 }
 
+//TODO: this is not good, since refreshing the page will load the default first card
 function discardChange(){
     console.log("abandon change");
+    location.reload();
 
+}
+
+function loadCardContent(sel){
+    var cardId = sel.value;
+    console.log(cardId);
+
+    $.ajax({
+        type : 'POST',
+        url : "./../../push_panel_new.php",
+        data: {'cardId':cardId},
+        beforeSend : function() {
+        },
+        success : function(result) {
+            $('form').replaceWith($(result).find('form'));
+            console.log($(result).find('div.wrapper'));
+        },
+        error : function() {
+        }
+    });
 }
