5ce7eee3a1313f84d3f808e522de98c0a310c520
qdx <qdxzzz@gmail.com>
2013-08-28 21:59:55 -0700

    added beta text to nav bar

--- admin.php
+++ admin.php
@@ -6,19 +6,20 @@ if (!isset($_SESSION['status']) || $_SESSION['status'] != "In") {
 require_once (dirname(__file__) . "/../lib/class/DataInterface.php");
 require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="UTF-8">
     <title></title>
-    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="src/bootstrap/css/bootstrap-fileupload.min.css" />
+    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="src/bootstrap/css/bootstrap-fileupload.min.css"/>
     <script type="text/javascript" src="src/scripts/jquery.min.js"></script>
     <script type="text/javascript" src="src/bootstrap/js/bootstrap-fileupload.min.js"></script>
     <script type="text/javascript" src="src/scripts/admin_panel.js"></script>
     <!-- include BlockUI -->
     <script src="src/scripts/jquery.blockUI.js"></script>
-    <link rel="stylesheet" href="src/style/push_panel.css" />
+    <link rel="stylesheet" href="src/style/push_panel.css"/>
 </head>
 <body>
 
@@ -34,15 +35,19 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                     <span class="icon-bar"></span>
                 </a>
                 <!-- Be sure to leave the brand out there if you want it shown -->
-                <ul class="nav"><li class="active"><a class="brand" href="index.html">iPassStore</a></li></ul>
+                <ul class="nav">
+                    <li class="active"><a class="brand" href="index.html">iPassStore</a></li>
+                </ul>
                 <ul class="nav">
                     <li><a href="./PassDesigner/index.php">PassDesigner</a></li>
                 </ul>
-
                 <ul class="pull-right">
                     <form action="lib/ws/login.php" class="navbar-form" method="post">
                         <input name="action" value="Logout" type="submit" class="btn btn-primary"/>
                     </form>
+                </ul>
+                <ul class="pull-right nav">
+                    <li class="active"><a>Beta</a></li>
                 </ul>
                 <!-- Everything you want hidden at 940px or less, place within here -->
                 <div class="nav-collapse collapse">
@@ -54,7 +59,7 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
     </div>
     <?php
     //TODO: add reset password panel
-    if (isset($_GET['message'])){
+    if (isset($_GET['message'])) {
         $message = $_GET['message'];
         echo "<h4 class=\"warning-message\">$message</h4>";
     }
@@ -71,14 +76,14 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                     print "<p style=\"text-align:center;\"><strong class=\"warning-message\">No cards created yet!</strong></p>";
                 } else {
                     $defaultCardId = -1;
-                    if(isset($_REQUEST['cardId']) && $_REQUEST['cardId'] != "" && in_array($_REQUEST['cardId'], $options))
+                    if (isset($_REQUEST['cardId']) && $_REQUEST['cardId'] != "" && in_array($_REQUEST['cardId'], $options))
                         $defaultCardId = $_REQUEST['cardId'];
                     print "<select id='selectCardId' name='cardId' onChange=\"loadCardContent(this)\">";
                     foreach ($options as $cardName => $cardId) {
-                        if($defaultCardId == -1){
+                        if ($defaultCardId == -1) {
                             $defaultCardId = $cardId;
                         }
-                        if($cardId == $defaultCardId)
+                        if ($cardId == $defaultCardId)
                             print "<option value='$cardId' selected>$cardName</option>";
                         else
                             print "<option value='$cardId'>$cardName</option>";
@@ -91,7 +96,7 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
 
                 }
                 $startIndex = 0;
-                if($cardDistributionType == "unique"){
+                if ($cardDistributionType == "unique") {
                     $startIndex = 1;
                 }
                 ?>
@@ -123,18 +128,19 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                     <label class="lengend-label" for="">Title Label</label>
                     <?php
                     $headerFields_label = $jsonContent["$cardType"]["headerFields"][0]["label"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_headerFields_0_label\" value=\"$headerFields_label\"/>";
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_headerFields_0_label\" value=\"$headerFields_label\"/>";
                     ?>
                 </div>
                 <div id="headerFields_value-wrapper">
                     <label class="lengend-label" for="">Title Content</label>
                     <?php
                     $headerFields_value = $jsonContent["$cardType"]["headerFields"][0]["value"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_headerFields_0_value\" value=\"$headerFields_value\"/>";
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_headerFields_0_value\" value=\"$headerFields_value\"/>";
                     ?>
                 </div>
                 <div id="strip-wrapper">
                     <label class="lengend-label" for="">Main Picture</label>
+
                     <div class="fileupload fileupload-new" data-provides="fileupload">
                         <span class="btn btn-file disabled">
                             <span class="fileupload-new">Select file</span>
@@ -148,28 +154,28 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                     <label class="lengend-label" for="">Section 1 Label</label>
                     <?php
                     $auxiliaryFields_0_label = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex]["label"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_auxiliaryFields_".$startIndex."_label\" value=\"$auxiliaryFields_0_label\"/>";
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_auxiliaryFields_" . $startIndex . "_label\" value=\"$auxiliaryFields_0_label\"/>";
                     ?>
                 </div>
                 <div id="auxiliaryField0_value-wrapper">
                     <label class="lengend-label" for="">Section 1 Content</label>
                     <?php
                     $auxiliaryFields_0_value = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex]["value"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_auxiliaryFields_".$startIndex."_value\" value=\"$auxiliaryFields_0_value\"/>";
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_auxiliaryFields_" . $startIndex . "_value\" value=\"$auxiliaryFields_0_value\"/>";
                     ?>
                 </div>
                 <div id="auxiliaryField1_label-wrapper">
                     <label class="lengend-label" for="">Section 2 Label</label>
                     <?php
-                    $auxiliaryFields_1_label = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex+1]["label"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_auxiliaryFields_".($startIndex+1)."_label\" value=\"$auxiliaryFields_1_label\"/>";
+                    $auxiliaryFields_1_label = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex + 1]["label"];
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_auxiliaryFields_" . ($startIndex + 1) . "_label\" value=\"$auxiliaryFields_1_label\"/>";
                     ?>
                 </div>
                 <div id="auxiliaryField1_value-wrapper">
                     <label class="lengend-label" for="">Section 2 Content</label>
                     <?php
-                    $auxiliaryFields_1_value = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex+1]["value"];
-                    print " <input class=\"content-input\" type=\"text\" name=\"json_".$cardType."_auxiliaryFields_".($startIndex+1)."_value\" value=\"$auxiliaryFields_1_value\"/>";
+                    $auxiliaryFields_1_value = $jsonContent["$cardType"]["auxiliaryFields"][$startIndex + 1]["value"];
+                    print " <input class=\"content-input\" type=\"text\" name=\"json_" . $cardType . "_auxiliaryFields_" . ($startIndex + 1) . "_value\" value=\"$auxiliaryFields_1_value\"/>";
                     ?>
                 </div>
                 <div id="barcode_message-wrapper">
@@ -185,13 +191,13 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                         <?php
                         $tmpVar = $jsonContent["barcode"]["format"];
 
-                        $barcode_format_map = array("PKBarcodeFormatQR"=>"",
-                            "PKBarcodeFormatPDF417"=>"",
-                            "PKBarcodeFormatAztec"=>"",
-                            "None"=>"");
-                        if(array_key_exists($tmpVar, $barcode_format_map)){
+                        $barcode_format_map = array("PKBarcodeFormatQR" => "",
+                            "PKBarcodeFormatPDF417" => "",
+                            "PKBarcodeFormatAztec" => "",
+                            "None" => "");
+                        if (array_key_exists($tmpVar, $barcode_format_map)) {
                             $barcode_format_map[$tmpVar] = "selected";
-                        }else{
+                        } else {
                             $barcode_format_map["None"] = "selected";
                         }
                         $qr = $barcode_format_map["PKBarcodeFormatQR"];
@@ -212,7 +218,9 @@ require_once (dirname(__file__) . "/../lib/class/JsonInterface.php");
                     print " <input class=\"content-input\" type=\"text\" name=\"json_barcode_altText\" value=\"$barcode_altText\"/>";
                     ?>
                 </div>
-                <img class="lengend-sample-picture" src="resource/image/storecard-front-lengend.png" alt="" style="width:800px;" />
+                <img class="lengend-sample-picture" src="resource/image/storecard-front-lengend.png" alt=""
+                     style="width:800px;"/>
+
                 <div id="SampleLabel"><label>Sample</label></div>
             </div>
         </div>
