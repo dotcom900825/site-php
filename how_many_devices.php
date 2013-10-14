d44e9fb23d8fd4c9feba2c198604ae3a9ebd2b10
qdx <qdxzzz@gmail.com>
2013-10-14 15:06:14 -0700

    add count devices

---
+++ how_many_devices.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once (dirname(__file__) . "/../lib/class/DataInterface.php");
+
+$pass = $_GET['pass'];
+$pool = array(168,110);
+if(in_array(intval($pass),$pool)){
+    $cardId = DataInterface::getCardIdByPassId($pass);
+    $cardName = DataInterface::$folderByCardId[intval($cardId)];
+    $numOfDevices = DataInterface::getDevicesCount($pass);
+    echo "<h1>we now have ".$numOfDevices." using ".$cardName."!!!</h1>";
+}else{
+    echo "<h3>invalid param!</h3>";
+}
