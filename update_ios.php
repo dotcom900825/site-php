891946ab14e864c966443e35f565b485c29e57ba
qdx <qdxzzz@gmail.com>
2013-10-29 17:20:07 -0800

    recognize old ios data records

---
+++ update_ios.php
@@ -0,0 +1,17 @@
+<?php
+require_once (dirname(__file__) . "/../lib/class/Database.php");
+
+$db = Database::get();
+$statement = $db->prepare("update devices set device_type = 'ios_old' where ID = ?");
+
+$handle = fopen("result.txt", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $id = trim($line);
+        $statement->execute(array($id));
+    }
+} else {
+    echo "error!";
+}
+echo "complete!";
+
