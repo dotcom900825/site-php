cb3671b4452b1e6b371f001f136ea529ac144f23
qdx <qdxzzz@gmail.com>
2013-11-02 12:30:13 -0800

    fix logic

--- business/v1/validate.php
+++ business/v1/validate.php
@@ -8,11 +8,15 @@ $org_id = $payload['organization_id'];
 $serial = $payload['serial_number'];
 
 $legal_serial = array(1);
-$legal_data = array(5=>$legal_serial);
+$legal_data = array(5 => $legal_serial);
 
 header('Content-type: application/json');
-if (in_array($org_id,$legal_data) && in_array($legal_data[$org_id], $serial)) {
-    echo json_encode(array('status' => 200, 'text' => "success"));
+if (in_array($org_id, $legal_data)) {
+    if (in_array($legal_data[$org_id], $serial)) {
+        echo json_encode(array('status' => 200, 'text' => "success"));
+    } else {
+        echo json_encode(array('status' => 200, 'text' => "failed"));
+    }
 } else {
     echo json_encode(array('status' => 404));
 }
