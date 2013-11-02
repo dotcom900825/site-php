78b0886386f86f10149f53b82a2f839f3b33ebca
qdx <qdxzzz@gmail.com>
2013-11-02 12:26:43 -0800

    added validation logic and fake data

--- business/v1/validate.php
+++ business/v1/validate.php
@@ -7,9 +7,12 @@ $payload = json_decode(file_get_contents('php://input'), true);
 $org_id = $payload['organization_id'];
 $serial = $payload['serial_number'];
 
+$legal_serial = array(1);
+$legal_data = array(5=>$legal_serial);
+
 header('Content-type: application/json');
-if ($org_id == 5) {
-    echo json_encode(array('status' => 200, 'text' => "I got: $serial"));
+if (in_array($org_id,$legal_data) && in_array($legal_data[$org_id], $serial)) {
+    echo json_encode(array('status' => 200, 'text' => "success"));
 } else {
     echo json_encode(array('status' => 404));
 }
