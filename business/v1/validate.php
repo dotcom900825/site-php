e7feb812975768a6564f188fb6ceb557e739d89b
ipa.production <dev@ipassstore.com>
2013-11-09 22:23:49 +0000

    optimize ucla pss

--- business/v1/validate.php
+++ business/v1/validate.php
@@ -1,22 +1,21 @@
 <?php
 //organization_id ----- organization's id retrieved from server
 //serial_number ----- serial number scanned from pass
-$payload = json_decode(file_get_contents('php://input'), true);
 
 
-$org_id = $payload['organization_id'];
-$serial = $payload['serial_number'];
+$org_id = $_POST['organization_id'];
+$serial = $_POST['serial_number'];
 
 $legal_serial = array(1);
 $legal_data = array(5 => $legal_serial);
 
 header('Content-type: application/json');
 if (array_key_exists($org_id, $legal_data)) {
-    if (in_array($serial, $legal_data[$org_id])) {
+    //if (in_array($serial, $legal_data[$org_id])) {
         echo json_encode(array('status' => 200, 'text' => "success"));
-    } else {
-        echo json_encode(array('status' => 200, 'text' => "failed"));
-    }
+    //} else {
+        //echo json_encode(array('status' => 200, 'text' => "failed"));
+    //}
 } else {
     echo json_encode(array('status' => 404));
 }
